e60822c60bc4832b8d7b17bf690eaaf6e7f8f5db
Marco0198 <219049505@mycput.ac.za>
2021-03-31 09:59:30 +0200

    this second commit

---
+++ src/test/java/za/ac/cput/StudentTest.java
@@ -0,0 +1,47 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class StudentTest {
+    private Student student1;
+    private Student student2;
+    private Student student3;
+
+    StudentTest() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        this.student1 = new Student();
+        this.student2 = new Student();
+        this.student3 = new Student();
+        this.student1 = this.student2;
+    }
+
+    // this is  test for object identuty
+    @Test
+    public void testIdentity() {
+        Assertions.assertSame(this.student1, this.student2);
+    }
+  //this is test for object  equality
+    @Test
+    public void testEquality() {
+        Assertions.assertEquals(this.student1, this.student2);
+    }
+
+   //  this is the test for test fail
+    @Test
+    public void testFailor() {
+        Assertions.assertEquals(this.student1, this.student2);
+        Assertions.fail("this will faill");
+    }
+  //this is the  test for Disabling Tests
+    @Disabled("Disabled until CustomerService is up!")
+    @Test
+    void testCustomerServiceGet() {
+        Assertions.assertEquals(this.student1, this.student2);
+    }
+}
